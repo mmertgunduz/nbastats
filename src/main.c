77f7d49b8e7c46ec54ac644179230511ea44a947
Mehmet Mert Gunduz <merttgg@gmail.com>
2025-08-06 22:56:03 +0300

    add indiana pacers logo

--- src/main.c
+++ src/main.c
@@ -37,7 +37,7 @@
  * @return int Exit status of the program.
  */
 int main(int argc, char *argv[]) {
-    printf("%s", get_pistons_logo());
+    printf("%s", get_pacers_logo());
 
     return 0;
 }
